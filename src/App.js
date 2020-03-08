a4da4154d6042de4a913eb6ba8ee6312434126aa
Igor <igrik12@hotmail.com>
2020-03-08 20:11:59 +0000

    Added developer role type

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Management from './components/Pages/Management/Management';
 import Layout from './components/Layout';
 import { isDeveloper } from './utils/permissions';
 import { Auth, API, graphqlOperation, Storage } from 'aws-amplify';
-import { listStaffs, listClients } from './graphql/queries';
+import { listStaffs, listClients, getStaff } from './graphql/queries';
 import { createStaff, createClient } from './graphql/mutations';
 
 const fakeClients = ['Bob', 'John', 'George', 'Amy'];
@@ -27,6 +27,7 @@ function App() {
   const themeColor = useStoreState(state => state.layoutModel.themeColor);
   const setUserGroups = useStoreActions(actions => actions.setUserGroups);
   const userGroups = useStoreState(state => state.userGroups);
+  const setStaff = useStoreActions(actions => actions.setStaff);
   const getStaff = useStoreActions(actions => actions.getStaff);
   const theme = useMemo(() => {
     return createMuiTheme({
@@ -65,36 +66,36 @@ function App() {
   useEffect(() => {
     const groups = Auth.user.signInUserSession.accessToken.payload['cognito:groups'];
     setUserGroups(groups);
-    getStaff(Auth.user.username);
   }, []);
 
-  // This is temporary hack for development user management
-  // useEffect(() => {
-  //   const apiCall = async () => {
-  //     const user = Auth.user;
-  //     getStaff(Auth.user.username);
-  //     const groups = user.signInUserSession.accessToken.payload['cognito:groups'];
-  //     let userType = 'user';
-  //     if (groups.length === 1 && groups[0] === 'developer') {
-  //       userType = 'developer';
-  //     }
-  //     const filterCondition = { filter: { userName: { eq: Auth.user.username } } };
-  //     const result = await API.graphql(graphqlOperation(listStaffs, filterCondition));
-  //     if (!result.data.listStaffs.items.length) {
-  //       const inputDetails = { input: { userName: Auth.user.username, userType: userType } };
-  //       await API.graphql(graphqlOperation(createStaff, inputDetails));
-  //     }
-  //     // Temp to populate fake clients to DB
-  //     const res = await API.graphql(graphqlOperation(listClients));
-  //     if (!res.data.listClients.items.length) {
-  //       fakeClients.forEach(async client => {
-  //         const clientDetails = { input: { name: client } };
-  //         await API.graphql(graphqlOperation(createClient, clientDetails));
-  //       });
-  //     }
-  //   };
-  //   apiCall();
-  // }, []);
+  //This is temporary hack for development user management
+  useEffect(() => {
+    const apiCall = async () => {
+      const user = Auth.user;
+      const groups = user.signInUserSession.accessToken.payload['cognito:groups'];
+      let userType = groups[0];
+
+      const filterCondition = { filter: { userName: { eq: Auth.user.username } } };
+      const result = await API.graphql(graphqlOperation(listStaffs, filterCondition));
+      if (!result.data.listStaffs.items.length) {
+        const inputDetails = { input: { userName: Auth.user.username, userType: userType } };
+        const ret = await API.graphql(graphqlOperation(createStaff, inputDetails));
+        setStaff(ret.data.createStaff);
+      } else {
+        getStaff(Auth.user.username);
+      }
+
+      // Temp to populate fake clients to DB
+      const res = await API.graphql(graphqlOperation(listClients));
+      if (!res.data.listClients.items.length) {
+        fakeClients.forEach(async client => {
+          const clientDetails = { input: { name: client } };
+          await API.graphql(graphqlOperation(createClient, clientDetails));
+        });
+      }
+    };
+    apiCall();
+  }, []);
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseLine />
